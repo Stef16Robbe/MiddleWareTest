9679e830c6a782f4c2a134bfda553ca3b744be61
Stef <stef.robbe@gmail.com>
2021-11-03 11:46:24 +0100

    Initial commit

---
+++ MiddleWareTest/ExceptionLoggingMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Middleware;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiddleWareTest
+{
+    public class ExceptionLoggingMiddleware : IFunctionsWorkerMiddleware
+    {
+        public async Task Invoke(FunctionContext context, FunctionExecutionDelegate next)
+        {
+            try
+            {
+                // Code before function execution here
+                await next(context);
+                // Code after function execution here
+            }
+            catch (Exception ex)
+            {
+                var log = context.GetLogger<ExceptionLoggingMiddleware>();
+                log.LogWarning(ex, string.Empty);
+            }
+        }
+    }
+}
---
+++ MiddleWareTest/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.Azure.Functions.Worker.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+
+namespace MiddleWareTest
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var host = new HostBuilder()
+                .ConfigureFunctionsWorkerDefaults(
+                    builder =>
+                    {
+                        builder.UseMiddleware<ExceptionLoggingMiddleware>();
+                    }
+                )
+                .Build();
+
+            host.RunAsync();
+        }
+    }
+}
